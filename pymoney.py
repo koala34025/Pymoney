2c3a4997ceba0a99e8a85a15a38bec8e6f0238d9
unknown <koala34025@gmail.com>
2022-09-28 17:03:53 +0800

    Add consecutive input feature

--- pymoney.py
+++ pymoney.py
@@ -1,4 +1,7 @@
 balance = int(input("How much money do you have? "))
-record = input("Add an expense or income record with description and amount:\n")
-balance += int(record.split()[1])
-print(f"Now you have {balance} dollars.")
+while True:
+    record = input("Add an expense or income record with description and amount (Enter 0 to exit):\n")
+    if record == '0':
+        break
+    balance += int(record.split()[1])
+    print(f"Now you have {balance} dollars.")
