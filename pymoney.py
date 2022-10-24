5b681f5b411ff6095afd2b88efb950f431866e34
unknown <koala34025@gmail.com>
2022-10-25 00:21:34 +0800

    Rearrange input() placement; Complete "File operations"

--- pymoney.py
+++ pymoney.py
@@ -1,23 +1,14 @@
 # Global variables
-overall_records = [] # Store the records in a list
-balance = 0 
+overall_records = [] # Store the records in a list of lists
+init_money = 0
 
-# Append a new record and calculate new balance
-def add():
+# Append a new record
+def add_rec(desc, amt):
     global overall_records
     global balance
     
-    # User input a record
-    record = input("Add an expense or income record with description and amount:\n")
-    
-    # Split the record into desc and amt
-    desc, amt = record.split()
-    
-    # Append the new record(a list of one list of two) to overall_records
+    # Append the new record to overall_records
     overall_records += [[desc, amt]]
-    
-    # Calculate new balance
-    balance += int(amt)
 
 # List all the current records
 def view_rec():
@@ -38,42 +29,72 @@ def view_rec():
     
     print('=== ==================== ======')
 
-# Report the balance so far to the user
+# Calculate and report the balance so far to the user
 def view_bal():
+    balance = init_money
+    
+    for desc, amt in overall_records:
+        balance += int(amt)
+    
     print(f'Now you have {balance} dollars.')
-
+    print()
+    
 # Remove a record
-def delete():
+def delete_rec(want_del):
     global overall_records
     global balance
 
-    # User input a record that will be deleted
-    want_del = int(input("Which record do you want to delete (0 to skip)? No."))
-    
-    if want_del == 0:
-        return False
-
     want_del -= 1 # Need adjustment becuase no. is 1 based and index is 0 based
-    
-    # Remove the amount of this record from the balance
-    balance -= int(overall_records[want_del][1])
     
     # Delete the record by concating former and latter list
     overall_records = overall_records[:want_del] + overall_records[want_del+1:]
     
-# User input initial amount of moeny
-balance = int(input('How much money do you have? '))
-
+try:
+    fh = open('records.txt', 'r')
+    
+    first_line = fh.readline()
+    init_money = int(first_line.split(',')[1][:-1])
+    
+    for desc_cma_amt_nl in fh.readlines():
+        desc, amt_nl = desc_cma_amt_nl.split(',')
+        amt = amt_nl[:-1]
+        
+        add_rec(desc, amt)
+    
+    fh.close()
+    
+    print('Welcome back!\n')
+    
+except Exception as e:
+    print(e)
+    # User input initial amount of money
+    init_money = int(input('How much money do you have? '))
+    print()
+    
 # User input a command consecutively
 while True:
     cmd = input('What do you want to do (add / view / delete / exit)? ')
     
     if cmd == 'add':
-        add()
+        record = input("Add an expense or income record with description and amount:\n")
+        print()
+        desc, amt = record.split()
+        add_rec(desc, amt)
+        
     elif cmd == 'view':
         view_rec()
         view_bal()
+        
     elif cmd == 'delete':
-        delete()
+        want_del = int(input("Which record do you want to delete (0 to skip)? No."))
+        if want_del == 0:
+            continue
+        delete_rec(want_del)
+        
     elif cmd == 'exit': # Break when user enters exit
+        with open('records.txt', 'w') as fh:
+            fh.write(f'init,{init_money}\n')
+            fh.writelines(desc + ',' + amt + '\n' for desc, amt in overall_records)
+        
+        print()
         break
