4916d79bc5e0f489531e8c85c9c30f23260f9d21
unknown <koala34025@gmail.com>
2022-10-25 11:47:21 +0800

    Complete some of the error handling part.

--- pymoney.py
+++ pymoney.py
@@ -1,15 +1,31 @@
+import sys
+
 # Global variables
 overall_records = [] # Store the records in a list of lists
 init_money = 0
+
+def initialize():
+    try:
+        # User input initial amount of money
+        init_money = int(input('How much money do you have? '))
+        print()
+    except ValueError:
+        init_money = 0
+        sys.stderr.write('Invalid value for money. Set to 0 by default.\n\n') 
 
 # Append a new record
 def add_rec(desc, amt):
     global overall_records
     global balance
     
-    # Append the new record to overall_records
-    overall_records += [[desc, amt]]
-
+    try:
+        int(amt)
+        # Append the new record to overall_records
+        overall_records += [[desc, amt]]
+    except ValueError:
+        sys.stderr.write('Invalid value for money.\n')
+        sys.stderr.write('Fail to add a record.\n\n')
+    
 # List all the current records
 def view_rec():
     global overall_records
@@ -44,6 +60,13 @@ def delete_rec(want_del):
     global overall_records
     global balance
 
+    if want_del == 0:
+        return
+    
+    if want_del < 0 or want_del > len(overall_records):
+        sys.stderr.write(f'There\'s no record with No.{want_del}. Fail to delete a record.\n\n')
+        return
+    
     want_del -= 1 # Need adjustment becuase no. is 1 based and index is 0 based
     
     # Delete the record by concating former and latter list
@@ -64,33 +87,41 @@ try:
     fh.close()
     
     print('Welcome back!\n')
-    
-except Exception as e:
-    print(e)
-    # User input initial amount of money
-    init_money = int(input('How much money do you have? '))
-    print()
+
+except FileNotFoundError:
+    initialize()
+        
+except IndexError:
+    sys.stderr.write('Invalid format in records.txt. Deleting the contents.\n')
+    initialize()
     
 # User input a command consecutively
 while True:
     cmd = input('What do you want to do (add / view / delete / exit)? ')
     
     if cmd == 'add':
-        record = input("Add an expense or income record with description and amount:\n")
-        print()
-        desc, amt = record.split()
-        add_rec(desc, amt)
+        try:
+            record = input("Add an expense or income record with description and amount:\n")
+            print()
+            desc, amt = record.split()
+            add_rec(desc, amt)
+            
+        except ValueError:
+            sys.stderr.write('The format of a record should be like this: breakfast -50.\n')
+            sys.stderr.write('Fail to add a record.\n\n')    
         
     elif cmd == 'view':
         view_rec()
         view_bal()
         
     elif cmd == 'delete':
-        want_del = int(input("Which record do you want to delete (0 to skip)? No."))
-        if want_del == 0:
-            continue
-        delete_rec(want_del)
-        
+        try:
+            want_del = int(input("Which record do you want to delete (0 to skip)? No."))
+            delete_rec(want_del)
+        except ValueError:
+            sys.stderr.write('Invalid format. Fail to delete a record.\n\n')
+            
+            
     elif cmd == 'exit': # Break when user enters exit
         with open('records.txt', 'w') as fh:
             fh.write(f'init,{init_money}\n')
@@ -98,3 +129,6 @@ while True:
         
         print()
         break
+    
+    else:
+        sys.stderr.write('Invalid command. Try again.\n\n')
